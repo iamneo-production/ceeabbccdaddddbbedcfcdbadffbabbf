17792fd5498733b9a0b835f834d08239dbba812a
Iamneo <techadmin@iamneo.ai>
2023-08-11 13:36:59 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -1,2 +1,9 @@
 import React from "react";
-import {}
+import { useState, useEffect } from "react";
+export default function Stopwatch() {
+    const [hour, Sethour] = useState(0);
+    const [min, Setmin] = useState(0);
+    const [sec, setsec] = useState(0);
+    const [stop, setStop] = useState()
+
+}
