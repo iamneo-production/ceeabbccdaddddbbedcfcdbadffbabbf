4911d44a262410a2949be0611608ba0de2e3997a
Iamneo <techadmin@iamneo.ai>
2023-08-11 13:41:59 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -4,6 +4,15 @@ export default function Stopwatch() {
     const [hour, Sethour] = useState(0);
     const [min, Setmin] = useState(0);
     const [sec, setsec] = useState(0);
-    const [stop, setStop] = useState()
+    const [stop, setStop] = useState(true);
+    const [dis, setdis] = useState(true);
+    const [isstart, setstart] = useState(true);
+    const [ispause, setpause] = useState(false);
+    const onStart = () => {
+        setStop(false);
+        setdis(false);
+        setstart(false);
+    };
+    const onStop
 
 }
