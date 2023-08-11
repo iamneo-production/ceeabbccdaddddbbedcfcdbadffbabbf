8f11f15a782c50aba6bcf3ac6de2fc4b2a5384bd
Iamneo <techadmin@iamneo.ai>
2023-08-11 14:21:58 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.jsx
+++ reactapp/src/components/Stopwatch/Stopwatch.jsx
@@ -60,7 +60,13 @@ export default function Stopwatch() {
          <p data-testid="time">
             {hour < 10 ? "0" + hour : hour}:
             {min < 10 ? "0" + min : min}:{" "}
+            {sec < 10 ? "0" + sec : sec}
          </p>
+         {isstart === false ? (
+            ispause === true ? (
+                <button data-testid="resume"onClick={onResume}>r</button>esume
+            )
+         )}
         </div>
     )
 }
